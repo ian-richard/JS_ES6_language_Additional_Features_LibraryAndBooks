6089f441d3c4c65797c73bdb4ef21e3db9965971
Ian F <ian.fiddeman@gmail.com>
2021-01-24 12:08:40 +0000

    initial commit

---
+++ library.js
@@ -0,0 +1,35 @@
+
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  bookCount(){
+    return this.books.length;
+  }
+
+  addBook(newBook){
+    this.books.push(newBook);}
+  // addBooks(newBooks){
+  //   newBooks.forEach(book => this.books.push(book));
+  // }
+  addBooks(newBooks) {
+    this.books = [...this.books, ...newBooks];
+  }
+
+  // printInventory(){
+  //   this.books.forEach((book) => {
+  //     console.log(`${book.title} by ${book.author}`);
+  //   });
+  // };
+
+  printInventory() {
+    this.books.forEach(({ author, title }) => {
+      console.log(`${title} by ${author}`);
+    });
+  }
+}
+
+export default Library;
+
+
